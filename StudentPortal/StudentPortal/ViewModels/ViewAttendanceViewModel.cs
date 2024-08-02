dd83448a9fe200da76b86f824da6771a0631b5cb
Yogesh Kumar <yogeshgopal98@gmail.com>
2024-08-02 15:11:33 -0400

    Attendance finetuning

--- StudentPortal/StudentPortal/ViewModels/ViewAttendanceViewModel.cs
+++ StudentPortal/StudentPortal/ViewModels/ViewAttendanceViewModel.cs
@@ -1,54 +1,21 @@
 ﻿using System.Collections.ObjectModel;
-using System.Windows.Input;
-using StudentPortal.Models;
 using StudentPortal.Services;
-using StudentPortal.Views;
 
 namespace StudentPortal.MVVM.ViewModels
 {
     public class ViewAttendanceViewModel : BaseViewModel
     {
-        public class ViewStudentsViewModel : BaseViewModel
+        private ObservableCollection<Models.Attendance> _attendanceRecords;
+
+        public ObservableCollection<Models.Attendance> AttendanceRecords
         {
-            private Student _selectedStudent;
+            get => _attendanceRecords;
+            set => SetProperty(ref _attendanceRecords, value);
+        }
 
-            public ObservableCollection<Student> Students { get; }
-            public Student SelectedStudent
-            {
-                get => _selectedStudent;
-                set => SetProperty(ref _selectedStudent, value);
-            }
-
-            public ICommand EditStudentCommand { get; }
-            public ICommand DeleteStudentCommand { get; }
-
-            public ViewStudentsViewModel()
-            {
-                Students = new ObservableCollection<Student>(Database.GetAllStudents());
-                EditStudentCommand = new RelayCommand(EditStudent, () => SelectedStudent != null);
-                DeleteStudentCommand = new RelayCommand(DeleteStudent, () => SelectedStudent != null);
-            }
-
-            private void EditStudent()
-            {
-                var editStudentView = new EditStudentView
-                {
-                    DataContext = new EditStudentViewModel(SelectedStudent)
-                };
-                editStudentView.ShowDialog();
-                Students.Clear();
-                foreach (var student in Database.GetAllStudents())
-                {
-                    Students.Add(student);
-                }
-            }
-
-            private void DeleteStudent()
-            {
-                Database.DeleteStudent(SelectedStudent);
-                Students.Remove(SelectedStudent);
-            }
+        public ViewAttendanceViewModel()
+        {
+            AttendanceRecords = new ObservableCollection<Models.Attendance>(Database.GetAllAttendance());
         }
     }
 }
-
--- StudentPortal/StudentPortal/obj/Debug/net8.0-windows/StudentPortal.AssemblyInfo.cs
+++ StudentPortal/StudentPortal/obj/Debug/net8.0-windows/StudentPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5883d6ef9713b221fafdb97a91200f669829e2c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cbd8588c0b0bd92584b5cbc282f7422f8913ebe")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
