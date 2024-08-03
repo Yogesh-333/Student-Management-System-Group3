d020a81238e1f037b456aa4625a4d542f86bd06b
Yogesh Kumar <yogeshgopal98@gmail.com>
2024-08-02 22:42:31 -0400

    Login page added and finetunings

---
+++ StudentPortal/StudentPortal/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StudentPortal.Helpers
+{
+    public static class PasswordBoxHelper
+    {
+        public static readonly DependencyProperty BoundPassword =
+            DependencyProperty.RegisterAttached("BoundPassword", typeof(string), typeof(PasswordBoxHelper), new PropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+        public static readonly DependencyProperty BindPassword =
+            DependencyProperty.RegisterAttached("BindPassword", typeof(bool), typeof(PasswordBoxHelper), new PropertyMetadata(false, OnBindPasswordChanged));
+
+        private static bool _isUpdating;
+
+        public static string GetBoundPassword(DependencyObject dp) => (string)dp.GetValue(BoundPassword);
+
+        public static void SetBoundPassword(DependencyObject dp, string value) => dp.SetValue(BoundPassword, value);
+
+        public static bool GetBindPassword(DependencyObject dp) => (bool)dp.GetValue(BindPassword);
+
+        public static void SetBindPassword(DependencyObject dp, bool value) => dp.SetValue(BindPassword, value);
+
+        private static void OnBoundPasswordChanged(DependencyObject dp, DependencyPropertyChangedEventArgs e)
+        {
+            if (dp is PasswordBox passwordBox && !_isUpdating)
+            {
+                passwordBox.PasswordChanged -= HandlePasswordChanged;
+                passwordBox.Password = (string)e.NewValue;
+                passwordBox.PasswordChanged += HandlePasswordChanged;
+            }
+        }
+
+        private static void OnBindPasswordChanged(DependencyObject dp, DependencyPropertyChangedEventArgs e)
+        {
+            if (dp is PasswordBox passwordBox)
+            {
+                if ((bool)e.OldValue)
+                {
+                    passwordBox.PasswordChanged -= HandlePasswordChanged;
+                }
+
+                if ((bool)e.NewValue)
+                {
+                    passwordBox.PasswordChanged += HandlePasswordChanged;
+                }
+            }
+        }
+
+        private static void HandlePasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                _isUpdating = true;
+                SetBoundPassword(passwordBox, passwordBox.Password);
+                _isUpdating = false;
+            }
+        }
+    }
+}
--- StudentPortal/StudentPortal/MainWindow.xaml.cs
+++ StudentPortal/StudentPortal/MainWindow.xaml.cs
@@ -4,10 +4,32 @@ namespace StudentPortal
 {
     public partial class MainWindow : Window
     {
+        private const string ValidUsername = "adam"; // Static username
+        private const string ValidPassword = "Test@123"; // Static password
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new MVVM.ViewModels.MainWindowViewModel();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (username == ValidUsername && password == ValidPassword)
+            {
+                // Successful login
+                LoginPanel.Visibility = Visibility.Collapsed;
+                MainOptionsPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                // Show error message
+                LoginErrorMessage.Visibility = Visibility.Visible;
+                LoginErrorMessage.Text = "Invalid username or password.";
+            }
         }
     }
 }
