4e02891e8e4a99f193ed6d62b1aabff6cad073ad
Hari Prasath Muthurathinam <hmuthurathinam0672@conestogac.on.ca>
2024-08-02 21:10:12 -0400

    Designing done

--- StudentPortal/StudentPortal/obj/Debug/net8.0-windows/StudentPortal.AssemblyInfo.cs
+++ StudentPortal/StudentPortal/obj/Debug/net8.0-windows/StudentPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2111ddaab616642d3713670d8c327c6d3ca7f16e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee4341e03059708c0e4bec401ad14dbbde9e749")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
