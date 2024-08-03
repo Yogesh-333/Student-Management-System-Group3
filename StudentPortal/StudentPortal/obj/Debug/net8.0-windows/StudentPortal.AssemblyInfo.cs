64cc5ddf3f5fbc95e1367426f65bdaea501c7610
Yogesh Kumar <yogeshgopal98@gmail.com>
2024-08-02 21:46:34 -0400

    Additional row issue fixed and App icon added

--- StudentPortal/StudentPortal/obj/Debug/net8.0-windows/StudentPortal.AssemblyInfo.cs
+++ StudentPortal/StudentPortal/obj/Debug/net8.0-windows/StudentPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee4341e03059708c0e4bec401ad14dbbde9e749")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e02891e8e4a99f193ed6d62b1aabff6cad073ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
